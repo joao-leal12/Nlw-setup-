826717270e954227a9c3adb43ddbb5a3156e1a59
João <joaovictorleal080415@gmail.com>
2023-01-19 17:08:47 -0300

    Finalizando homepage da aplicação we

--- web/web/src/App.tsx
+++ web/web/src/App.tsx
@@ -1,7 +1,14 @@
 import './styles/global.css'
 
+import { Header } from './components/header';
+import { SummaryTable } from './components/SummaryTable';
 export const App = () => {
   return (
-    <div className='bg-black h-100 text-white'>Hello World</div>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header/>
+        <SummaryTable/>
+      </div>
+    </div>
   )
 }
